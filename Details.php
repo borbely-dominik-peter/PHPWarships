ffc769bab918237fff1381b59214718771115cc6
borbely-dominik-peter <borbely.dominik.peter@students.jedlik.eu>
2025-06-02 10:22:48 +0200

    design 2

--- Details.php
+++ Details.php
@@ -22,72 +22,82 @@
     <title>Add New</title>
 </head>
 <body>
-    <form method="POST" action="<?php if ($_GET["mod"] == "true") {
-        echo "index.php?todo=mod";
-    }else {
-        echo "index.php?todo=add";
-    }?>">
-        <input type="number" name="id" id="id" hidden value="<?php 
-                if ($_GET["mod"] == "true" && $id != "") {
-                    echo $id;
-                }
-            ?>">
-        <div class="mb-3">
-            <label for="name" class="form-label">Name:</label>
-            <input type="text" class="form-control" id="name" name="name" value="<?php 
-                if ($_GET["mod"] == "true" && $name != "") {
-                    echo " $name ";
-                }
-            ?>">
-        </div>
-        <div class="mb-3">
-            <label for="class" class="form-label">Class:</label>
-            <input type="text" class="form-control" id="class" name="class" value="<?php 
-                if ($_GET["mod"] == "true" && $class != "") {
-                    echo " $class ";
-                }
-            ?>">
-        </div>
-        <div class="mb-3">
-            <select name="type" id="type">
-                <?php 
-                    $Options = ["Battleship", "Battlecruiser", "Armoured Cruiser", "Protected Cruiser", "Scout Cruiser", "Light Cruiser", "Destroyer", "Submarine"];
-                    foreach($Options as $opt){
-                        if ($_GET["mod"] == "true" && $opt == $type) {
-                            echo "<option value='$opt' selected >$opt</option>";
-                        }
-                        else{
-                            echo "<option value='$opt'>$opt</option>";
-                        }
+    <div class="container">
+        <?php 
+            if ($_GET["mod"] == "true") {
+                echo "<h1 class='text-center'>Modify Warship</h1>";
+            }
+            else{
+                echo "<h1 class='text-center'>Add New Warship</h1>";
+            }
+        ?>
+        <form method="POST" action="<?php if ($_GET["mod"] == "true") {
+            echo "index.php?todo=mod";
+        }else {
+            echo "index.php?todo=add";
+        }?>">
+            <input type="number" name="id" id="id" hidden value="<?php 
+                    if ($_GET["mod"] == "true" && $id != "") {
+                        echo $id;
                     }
-                ?>
-            </select>
-        </div>
-        <div class="mb-3">
-            <label for="launched" class="form-label">Launch Year:</label>
-            <input type="number" class="form-control" id="launched" name="launched" value="<?php 
-                if ($_GET["mod"] == "true" && $launched != "") {
-                    echo $launched;
-                }
-            ?>">
-        </div>
-        <div class="mb-3">
-            <label for="MGunCal" class="form-label">Main gun caliber:</label>
-            <input type="text" class="form-control" id="MGunCal" name="MGunCal" value="<?php 
-                if ($_GET["mod"] == "true" && $MGunCal != "") {
-                    echo " $MGunCal ";
-                }
-            ?>">
-        </div>
-        <div class="mb-3">
-            <label for="country" class="form-label">Country:</label>
-            <input type="text" class="form-control" id="country" name="country" value="<?php 
-                if ($_GET["mod"] == "true" && $country != "") {
-                    echo " $country ";
-                }
-            ?>">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+                ?>">
+            <div class="mb-3">
+                <label for="name" class="form-label">Name:</label>
+                <input type="text" class="form-control" id="name" name="name" value="<?php 
+                    if ($_GET["mod"] == "true" && $name != "") {
+                        echo " $name ";
+                    }
+                ?>">
+            </div>
+            <div class="mb-3">
+                <label for="class" class="form-label">Class:</label>
+                <input type="text" class="form-control" id="class" name="class" value="<?php 
+                    if ($_GET["mod"] == "true" && $class != "") {
+                        echo " $class ";
+                    }
+                ?>">
+            </div>
+            <div class="mb-3">
+                <select name="type" id="type">
+                    <?php 
+                        $Options = ["Battleship", "Battlecruiser", "Armoured Cruiser", "Protected Cruiser", "Scout Cruiser", "Light Cruiser", "Destroyer", "Submarine"];
+                        foreach($Options as $opt){
+                            if ($_GET["mod"] == "true" && $opt == $type) {
+                                echo "<option value='$opt' selected >$opt</option>";
+                            }
+                            else{
+                                echo "<option value='$opt'>$opt</option>";
+                            }
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="launched" class="form-label">Launch Year:</label>
+                <input type="number" class="form-control" id="launched" name="launched" value="<?php 
+                    if ($_GET["mod"] == "true" && $launched != "") {
+                        echo $launched;
+                    }
+                ?>">
+            </div>
+            <div class="mb-3">
+                <label for="MGunCal" class="form-label">Main gun caliber:</label>
+                <input type="text" class="form-control" id="MGunCal" name="MGunCal" value="<?php 
+                    if ($_GET["mod"] == "true" && $MGunCal != "") {
+                        echo " $MGunCal ";
+                    }
+                ?>">
+            </div>
+            <div class="mb-3">
+                <label for="country" class="form-label">Country:</label>
+                <input type="text" class="form-control" id="country" name="country" value="<?php 
+                    if ($_GET["mod"] == "true" && $country != "") {
+                        echo " $country ";
+                    }
+                ?>">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
 </body>
 </html>
